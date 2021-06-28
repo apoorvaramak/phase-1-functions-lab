6a0eba317334e0b468a6016f4481dbf44763cd9b
Apoorva Ramakrishnan <apoorvar@comcast.net>
2021-06-28 11:26:56 -0500

    commit tree 3504a4923ede93909ad583733d398e894adcaf6b

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(num){
+    return Math.abs(42-num);
+}
+
+function distanceFromHqInFeet(num){
+    return Math.abs(42-num) * 264;
+}
+
+function distanceTravelledInFeet(num1, num2){
+    return Math.abs(num1-num2) * 264; 
+}
+
+function calculatesFarePrice(num1, num2){
+    let distance = Math.abs(num1-num2) * 264; 
+    if(distance < 400){
+        return 0; 
+    }
+    else if(distance < 2000)
+    {
+        return (distance-400) * .02;
+    }
+    else if(distance < 2500){
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
